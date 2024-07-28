0dccc981105fd3ab795f58797e24c10b189c6593
Boudynab <legendboudy@gmail.com>
2024-07-29 01:30:56 +0300

    lol

--- Static_/Subsets_generation.cpp
+++ Static_/Subsets_generation.cpp
@@ -1,24 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-void generateSubsets(int n, int current, vector<int>& subset) {
+void generateSubsets(int n, int current, vector<int>& subset, vector<vector<int>>& all_subsets) {
     if (current > n) {
         if (!subset.empty()) {
-            for (int i = 0; i < subset.size(); ++i) {
-                cout << subset[i];
-            }
-            cout << endl;
+            all_subsets.push_back(subset);
         }
         return;
     }
     subset.push_back(current);
-    generateSubsets(n, current + 1, subset);
+    generateSubsets(n, current + 1, subset, all_subsets);
     subset.pop_back();
-    generateSubsets(n, current + 1, subset);
+    generateSubsets(n, current + 1, subset, all_subsets);
+}
+bool compare(const vector<int>& a, const vector<int>&b) {
+    if (a.size() != b.size()) {
+            return a.size()<b.size();
+        }
+        return a < b;
 }
 int main() {
     int n;
     cin >> n;
     vector<int> subset;
-    generateSubsets(n, 1, subset);
+    vector<vector<int>> all_subsets;
+    generateSubsets(n, 1, subset, all_subsets);
+    sort(all_subsets.begin(), all_subsets.end(),compare);
+    for (const auto& s : all_subsets) {
+        for (int num : s) {
+            cout << num;
+        }
+        cout << endl;
+    }
     return 0;
 }
