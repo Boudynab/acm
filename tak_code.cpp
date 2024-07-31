c3c2c15675e79fa1247e9ca5ea51ca299ff54cac
Boudynab <legendboudy@gmail.com>
2024-07-31 18:21:51 +0300

    lol

--- tak_code.cpp
+++ tak_code.cpp
@@ -1,70 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool isValidTaKCode(int x, int y, const vector<vector<char>>& grid) {
-    int n = grid.size();
-    int m = grid[0].size();
-    //top-left
-    for (int i=x;i<x+3;i++) {
-        for (int j=y;j<y+3;j++) {
-            if (i>=n||j>=m||grid[i][j]!='#'){
+
+bool isValidTaKCode(int i,int j,const vector<vector<char>>&grid) {
+    if (i+8>=grid.size()||j+8>=grid[0].size()) {
+        return false;
+    } //Top-left
+    for (int x=0;x<3;x++) {
+        for (int y=0;y<3;y++) {
+            if (grid[i+x][j+y]!='#') {
                 return false;
-        }
-}
-    }
-    //bottom-right
-    for (int i=x+6;i<x+9;i++) {
-        for (int j=y+6;j<y+9;j++) {
-            if (i>=n||j>=m||grid[i][j]!='#'){
-                return false;
-        }
-}
-    }
-    //top-left
-    for (int i=x-1;i<=x+3;i++) {
-        for (int j=y-1; j<=y+3;j++) {
-            if (i>=0&&i<n&&j>=0&&j<m) {
-                if(!(i>=x&&i<x+3&&j>=y&&j<y+3)) {
-                    if(grid[i][j]!='.'){
-                        return false;
-                }
-                }
             }
         }
     }
-    //bottom-right
-    for (int i=x+5;i<=x+9;i++) {
-        for (int j=y+5;j<=y+9;j++) {
-            if (i>=0&&i<n&&j>=0&&j<m) {
-                if (!(i>=x+6&&i<x+9&&j>=y+6&&j<y+9)) {
-                    if (grid[i][j]!='.'){
-                    return false;
-                }
-                }
+    // Bottom-right
+    for (int x=6;x<9;x++) {
+        for (int y=6;y<9;y++) {
+            if (grid[i+x][j+y]!='#') {
+                return false;
             }
+        }
+    }
+    vector<pair<int, int>>whiteCells = {
+        {0, 3}, {1, 3}, {2, 3},{3, 0}, {3, 1}, {3, 2}, {3, 3},{5, 5}, {5, 6}, {5, 7}, {5, 8},{6, 5}, {7, 5}, {8, 5}
+    };
+    for (const auto& cell : whiteCells) {
+        if (grid[i+cell.first][j+cell.second]!='.') {
+            return false;
         }
     }
     return true;
 }
-
 int main() {
     int n, m;
     cin >> n >> m;
     vector<vector<char>> grid(n, vector<char>(m));
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<m;j++) {
             cin >> grid[i][j];
         }
     }
     vector<pair<int, int>> results;
     for (int i=0;i<n;i++) {
-        for (int j =0;j<m;j++) {
-            if (isValidTaKCode(i, j, grid)) {
-                results.push_back({i + 1,j + 1});
+        for (int j=0;j<m;j++) {
+            if (isValidTaKCode(i,j,grid)) {
+                results.push_back({i+1,j+1});
             }
         }
     }
     for (const auto& result : results) {
-        cout << result.first<<" "<<result.second<<endl;
+        cout<<result.first<< " " <<result.second<<endl;
     }
-    return 0;
 }
